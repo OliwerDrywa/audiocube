5b4334f7d8410acaea66eab9abb14fc310b09968
OliwerDrywa <oliwerdrywa@gmail.com>
2021-07-06 02:19:58 +0100

    npm init @vitejs/app

---
+++ README.md
@@ -0,0 +1,18 @@
+# audiocube - a tutorial
+## to get stated
+0. install tools \
+   [svelte extension](https://marketplace.visualstudio.com/items?itemName=svelte.svelte-vscode) - syntax highlighting for `.svelte` files \
+   [prettier](https://marketplace.visualstudio.com/items?itemName=esbenp.prettier-vscode) - auto-format with `shift+alt+f`  
+1. clone the gihub repository \
+   in an new empty project in vscode terminal run \
+   ```git clone https://github.com/OliwerDrywa/audiocube.git .```
+2. install dependencies (svelte, ...) \
+   in same terminal run \
+   ```npm i```
+3. start the app \
+   ```npm start```
+4. open App.svelte and edit
+
+## tasks
+1. at least skim the [svelte tutorial](https://svelte.dev/tutorial/basics)
+2. mess around with the site's content
---
+++ src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+  import logo from "./assets/svelte.png";
+  import Counter from "./lib/Counter.svelte";
+</script>
+
+<main>
+  <!-- hint - <audio /> can be used similarly to <img /> -->
+  <img src={logo} alt="Svelte Logo" />
+  <h1>eło</h1>
+
+  <Counter />
+
+  <p>
+    oficjalny i zajebisty
+    <a href="https://svelte.dev/tutorial/basics">tutorial svelte</a>
+    polecam
+  </p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  img {
+    height: 16rem;
+    width: 16rem;
+  }
+
+  h1 {
+    color: #ff3e00;
+    font-size: 4rem;
+    font-weight: 100;
+    line-height: 1.1;
+    margin: 2rem auto;
+    max-width: 14rem;
+  }
+
+  p {
+    max-width: 14rem;
+    margin: 1rem auto;
+    line-height: 1.35;
+  }
+
+  a {
+    color: #ff3e00;
+  }
+</style>
